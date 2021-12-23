170d5ec95582096bc7789eba147aaae41fb1e7b9
Fituning <carstar@live.fr>
2021-12-23 14:24:13 +0100

    size image view

--- src/main/java/fr/utbm/info/ap4b_project_fx/HelloApplication.java
+++ src/main/java/fr/utbm/info/ap4b_project_fx/HelloApplication.java
@@ -3,6 +3,8 @@ package fr.utbm.info.ap4b_project_fx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,17 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("GameBoard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainMenu.fxml"));
+
+        BorderPane pane = new BorderPane();
+        ImageView img = new ImageView("C:\\Users\\carst\\Desktop\\Drive memes\\téléchargement (3).png");
+
+        img.fitWidthProperty().bind(stage.widthProperty());
+
+        //fxmlLoader.load().setCenter(img);
+        pane.setCenter(img);
+
+        Scene scene = new Scene(pane);
         stage.setFullScreen(true);
         stage.setScene(scene);
         stage.show();
